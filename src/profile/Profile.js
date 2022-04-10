a400bb18fd0a2250481bcbca2d54e916e13f42a3
Kasraoui Anouar <med.anouar91@gmail.com>
2022-04-10 16:21:40 +0200

    first commit

---
+++ src/profile/Profile.js
@@ -0,0 +1,15 @@
+const Profile=()=>{
+    return(
+        <div>
+            <img src= ""/>
+            <div>
+            <div>123</div>
+            <div></div>
+            <div></div>
+            <button>clic me</button>
+            </div>
+        </div>
+        
+    )
+}
+export default Profile;
